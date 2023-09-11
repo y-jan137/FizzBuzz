a0ed1b9adc2ff792ab90c6876c87210a2c10f6a7
y-jan137 <yousefjan24000@gmail.com>
2023-09-11 10:36:26 -0400

    Added Reduce and Multiples

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,19 @@
 public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int steps = 0;
+
+        while (n != 0) {
+
+            boolean b = (n % 2 == 0);
+            if (b) {
+                n /= 2;
+            }
+            else {
+                n = n-1;
+            }
+            steps += 1;
+        }
+        System.out.println(steps);
+    }
 }
